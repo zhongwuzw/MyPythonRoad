b53f6329ef32d7922e783e95c6d38826c1fdb7e7
zhongwuzw <zhongwuzw@qq.com>
2014-04-24 09:40:19 +0800

    New Add

--- src/test1.py
+++ src/test1.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
-import os,sys
-print sys.argv
+import urllib
+import chardet
+rawdata = urllib.urlopen('http://www.qq.com').read()
+tempcode = chardet.detect(rawdata)['encoding']
+rawdata = rawdata.decode(tempcode,'ignore')
+rawdata = rawdata.encode('utf-8')
+# rawdata = unicode(rawdata,tempcode).encode('utf8')
+print rawdata
+# import os,sys
+# print sys.argv
