d8571dea5311b824d552971a96af3639bae130d5
zhongwuzw <zhongwuzw@qq.com>
2014-05-02 10:04:46 +0800

    Edit

--- src/requeststest.py
+++ src/requeststest.py
@@ -1,6 +1,15 @@
 import requests
 import re
+from bs4 import BeautifulSoup
+
 headers = {"X-Requested-With":"XMLHttpRequest"}
 r = requests.get('http://bbs.byr.cn/board/Job/mode/6?_uid=guest',headers = headers)
 r.encoding = 'GBK'
-print r.text
+soup = BeautifulSoup(r.text)
+tagset = soup.find_all('a',attrs = {'href':re.compile('^/article/Job/\d+$')})
+templist = []
+for tag in tagset:
+    if tag.parent['class'][0] == 'title_9':
+        templist.append(tag)
+        print tag
+
--- src/temp.py
+++ src/temp.py
@@ -1,2 +1,22 @@
-import sys
-print sys.path
+import re
+str_temp = '{"name": "john", "smith", "paul", "address": "nyc", "chicago", "age": 50, 60}'
+index_temp = str_temp.split(',')
+temp_flag = 0
+temp_list = []
+for object in index_temp:
+    if object.find(':') != -1:
+        if temp_flag == 0:
+            temp_count = object.find(':')
+            object = object[:temp_count + 1] + '[' + object[temp_count + 1:]+','
+            temp_flag = 1;
+        else:
+            temp_count = object.find(':')
+            object = '],' + object[:temp_count + 1] + '[' + object[temp_count + 1:] + ','
+    else:
+        object = object + ',' 
+    temp_list.append(object)
+final_str = ''.join(temp_list)
+final_str = final_str[:-2] + ']}'
+print final_str
+        
+        
